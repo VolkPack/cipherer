22ec39f7a4a4844d7910d6efa9b8b02f7ac9d8dd
VolkPack <nikitakarayev@gmail.com>
2016-08-03 20:29:43 -0700

    Ceaser Cipher Functionality Added, Encode, Decode, Copy and Clear

--- app/src/main/java/com/example/volks/cypher/CeaserCipher.java
+++ app/src/main/java/com/example/volks/cypher/CeaserCipher.java
@@ -1,5 +1,7 @@
 package com.example.volks.cypher;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,21 +18,19 @@ public class CeaserCipher extends AppCompatActivity {
         setContentView(R.layout.activity_ceaser_cipher);
     }
 
-    public void EncodeText(View view)
-    {
+    public void EncodeText(View view) {
         int shift;
         String input;
-        String output="";
+        String output = "";
 
         EditText shift_text = (EditText) findViewById(R.id.shift_input);
         EditText ceaser_text = (EditText) findViewById(R.id.ceaser_input);
         TextView textOutput = (TextView) findViewById(R.id.output);
 
-        shift =  Integer.parseInt(shift_text.getText().toString());
+        shift = Integer.parseInt(shift_text.getText().toString());
         input = ceaser_text.getText().toString();
 
-        if (shift >= 1)
-        {
+        if (shift >= 1) {
 
             for (int i = 0; i < input.length(); i++) {
                 char tempChar = (input.charAt(i));
@@ -44,6 +44,70 @@ public class CeaserCipher extends AppCompatActivity {
                     }
                 }
             }
+        } else {
+            Context context = getApplicationContext();
+            String noShift = getString(R.string.noShift);
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, noShift, duration);
+            toast.show();
+        }
+        textOutput.setText(output);
+    }
+
+    public void ceaserClear(View view) {
+        EditText input = (EditText) findViewById(R.id.ceaser_input);
+        EditText shift = (EditText) findViewById(R.id.shift_input);
+        TextView output = (TextView) findViewById(R.id.output);
+
+        input.setText("");
+        output.setText("");
+        shift.setText("0");
+    }
+
+    public void ceaserCopy(View view) {
+        TextView output = (TextView) findViewById(R.id.output);
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("cipher", output.getText().toString());
+        clipboard.setPrimaryClip(clip);
+    }
+
+    public void decodeText(View view)
+    {
+        int shift;
+        String input;
+        String output = "";
+
+        EditText shift_text = (EditText) findViewById(R.id.shift_input);
+        EditText ceaser_text = (EditText) findViewById(R.id.ceaser_input);
+        TextView textOutput = (TextView) findViewById(R.id.output);
+
+        shift = Integer.parseInt(shift_text.getText().toString());
+        input = ceaser_text.getText().toString();
+
+        if (shift >= 1)
+        {
+
+                for(int i = 0; i < input.length(); i++)
+                {
+                    int c = input.charAt(i);
+                    if(Character.isUpperCase(c))
+                    {
+                        c = c -(shift%26);
+                        if(c < 'A')
+                        {
+                            c = c + 26;
+                        }
+                    }
+                    else if(Character.isLowerCase(c))
+                    {
+                        c = c - (shift%26);
+                        if(c <'a')
+                        {
+                            c = c + 26;
+                        }
+                    }
+                    output = output + (char)c;
+                }
         }
         else
         {
